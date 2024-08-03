299c08cc3b17dd6077b75dfb112def52a8cae4f7
Kavichelvan P <41743539+kavichelvan@users.noreply.github.com>
2024-08-03 22:34:59 +0530

    Interactive loader added (#9)
    * initial commit, cleanup done
    
    * updated readme to check git rebase in PR
    
    * UI api integrated (#2)
    
    * initial commit, cleanup done
    
    * updated readme to check git rebase in PR
    
    * cherry-pick file add test
    
    * cherry-pick test commit 2
    
    * Revert "UI api integrated (#2)" (#3)
    
    This reverts commit fdbc846b76d17cc7da7dd5446fb224008e3823dd.
    
    * initial commit, cleanup done, cherry pick test (#4)
    
    * initial commit, cleanup done, cherry pick test (#4) (#5)
    
    * Develop2 (#6)
    
    * updated readme to check git rebase in PR
    
    * initial commit, cleanup done
    
    * updated readme to check git rebase in PR
    
    * cherry-pick file add test
    
    * cherry-pick test commit 2
    
    * initial commit, cleanup done
    
    updated readme to check git rebase in PR
    
    UI api integrated (#2)
    
    * initial commit, cleanup done
    
    * updated readme to check git rebase in PR
    
    * cherry-pick file add test
    
    * cherry-pick test commit 2
    
    Revert "UI api integrated (#2)" (#3)
    
    This reverts commit fdbc846b76d17cc7da7dd5446fb224008e3823dd.
    
    initial commit, cleanup done, cherry pick test (#4)
    
    initial commit, cleanup done, cherry pick test (#4) (#5)
    
    * updated readme to check git rebase in PR
    
    * initial commit, cleanup done
    
    * updated readme to check git rebase in PR
    
    * cherry-pick file add test
    
    * cherry-pick test commit 2
    
    * updated readme to check git rebase in PR
    
    * initial commit, cleanup done
    
    * updated readme to check git rebase in PR
    
    * cherry-pick file add test
    
    * cherry-pick test commit 2
    
    * Lottie animator

--- src/App.js
+++ src/App.js
@@ -1,28 +1,35 @@
 // src/App.js
+// https://lottiefiles.com/
 
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import './App.css'
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
+import Lottie from 'lottie-react'
+import animationData from './animation.json'
 
 function App() {
-  const [data, setData] = useState(null);
+  const [data, setData] = useState(null)
 
   useEffect(() => {
-    axios.get('http://localhost:5000/api/data')
-      .then(response => {
-          setData(response.data);
-          //setData("");
-          console.log(data);
+    axios
+      .get('http://localhost:5000/api/data')
+      .then((response) => {
+        setData(response.data)
+        //setData("");
+        console.log(data)
       })
-      .catch(error => {
-        console.error('Error fetching data:', error);
-      });
-  }, []);
+      .catch((error) => {
+        console.error('Error fetching data:', error)
+      })
+  }, [])
 
   return (
     <div className="App">
       {data ? (
-          <div>
-            <h1>Data from Frontend</h1>
+        <div>
+          <h1>Data from Frontend</h1>
           <p>{data.message}</p>
           {/* <ul>
             {data.data.map((item, index) => (
@@ -31,10 +38,29 @@ function App() {
           </ul> */}
         </div>
       ) : (
-        <p>Loading data...</p>
+        // <div className="spinner"></div>
+        //     <div className="dots">
+        //   <div className="dot"></div>
+        //   <div className="dot"></div>
+        //   <div className="dot"></div>
+        // </div>
+        //   <div className="progress-bar-container">
+        // <div className="progress-bar"></div>
+        //   </div>
+        //     <div>
+        //   <h1><Skeleton width={300} /></h1>
+        //   <p><Skeleton count={3} /></p>
+        // </div>
+        <div className="loader-container">
+          <Lottie
+            animationData={animationData}
+            loop={true}
+            style={{ width: 300, height: 300 }}
+          />
+        </div>
       )}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
